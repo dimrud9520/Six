056272176b867c3bf11ae4ad54e6f7da5a6f677f
dimrud9520 <rudykan@bk.ru>
2021-07-11 18:54:51 +0300

    Исправленная задача

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -5,21 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarController {
+    private CarService carService=new CarServiceImpl();
 
     @GetMapping("/cars")
     public String carPage(@RequestParam(value = "count", required = false) Integer count, Model model) {
         if (count == null) {
-            model.addAttribute("carServ", Car.getCar(5));
+            model.addAttribute("carServ", carService.getCar(5));
         }
         if (count != null) {
             if (count >= 5) {
-                model.addAttribute("carServ", Car.getCar(5));
+                model.addAttribute("carServ", carService.getCar(5));
             } else {
-                model.addAttribute("carServ", Car.getCar(count));
+                model.addAttribute("carServ", carService.getCar(count));
             }
         }
         return "first/cars";
